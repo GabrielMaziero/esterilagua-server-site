693085b99becf16a0c16ee973ecdcabadf992718
Gabriel Maziero <gabriel.maziero@midway.com.br>
2022-12-22 18:27:23 -0300

    feat: created method send token recaptcha

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 const express = require("express");
+const cors = require('cors');
+const axios = require('axios');
+require('dotenv').config();
+
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.send("Olá Gabriel"));
+const corsOptions = {
+  origin: process.env.ORIGIN_CORS,
+  optionsSuccessStatus: 200
+}
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.use(cors())
+
+app.get("/recaptcha", async (req, res) => {
+  const { token } = req.query;
+  try {
+    const { data } = await axios.post(`https://www.google.com/recaptcha/api/siteverify?secret=${process.env.SECRET_KEY}&response=${token}`);
+    const { 'error-codes': error, success } = data;
+    if (!success) {
+      throw error[0]
+    }
+    return res.status(200).send({ success });
+  } catch (error) {
+    return res.status(404).send({ error })
+  }
+});
+
+app.listen(port);
